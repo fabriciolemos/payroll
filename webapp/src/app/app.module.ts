008e1f40d603131abc383f550a914555ecb24861
Fabricio Lemos <fabricio.lemos@gmail.com>
2018-09-06 21:11:06 -0400

    configure ngrx

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { PayrollReportComponent } from './payroll-report/payroll-report.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,10 @@ import { PayrollReportComponent } from './payroll-report/payroll-report.componen
     PayrollReportComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([AppEffects])
   ],
   providers: [],
   bootstrap: [AppComponent]
