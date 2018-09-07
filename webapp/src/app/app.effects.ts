406c29b2bea643106567abdce974c87a854514ca
Fabricio Lemos <fabricio.lemos@gmail.com>
2018-09-06 21:21:46 -0400

    add init effect action

--- webapp/src/app/app.effects.ts
+++ webapp/src/app/app.effects.ts
@@ -1,9 +1,17 @@
-import { Injectable } from '@angular/core';
-import { Actions, Effect } from '@ngrx/effects';
+import {Injectable} from '@angular/core';
+import {Actions, Effect, ofType, ROOT_EFFECTS_INIT} from '@ngrx/effects';
+import {PayrollService} from "./payroll-report/payroll.service";
+import {map, tap} from "rxjs/operators";
+import {defer, Observable, of} from "rxjs";
 
 
 @Injectable()
 export class AppEffects {
+  @Effect({dispatch: false})
+  init$: Observable<any> = defer(() => of(null)).pipe(
+    tap(() => this.payrollService.loadPayroll())
+  );
 
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions, private payrollService: PayrollService) {
+  }
 }
